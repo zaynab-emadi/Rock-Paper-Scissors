350e4fde3bda1473763206afa94452435e425d48
zaynab_emadi <zaynabemadi@gmail.com>
2025-05-05 21:40:20 +0330

    add scores and reset button

--- js/main.js
+++ js/main.js
@@ -1,27 +1,41 @@
-function playGame (value){
-    let playerOne = value;
-    let moves = ['rock', 'paper', 'scissors'];
-    let computerChoice = moves[Math.floor(Math.random() * moves.length)];
-    if(computerChoice === playerOne){
-        alert(`player one has ${playerOne}, computer has ${computerChoice} \nit's a tie`);
-    }
-    else if(playerOne === 'rock' && computerChoice === 'paper') {
-        alert(`player one has ${playerOne}, computer has ${computerChoice} \ncomputer wins`);
-    }
-    else if (playerOne === 'rock' && computerChoice === 'scissors') {
-        alert(`player one has ${playerOne}, computer has ${computerChoice} \nplayer one wins`);
-    }
-    else if (playerOne === 'paper' && computerChoice === 'scissors') {
-        alert(`player one has ${playerOne}, computer has ${computerChoice} \ncomputer wins`);
-    }
-    else if (playerOne === 'paper' && computerChoice === 'rock') {
-        alert(`player one has ${playerOne}, computer has ${computerChoice} \nplayer one wins`);
-    }
-    else if (playerOne === 'scissors' && computerChoice === 'paper') {
-        alert(`player one has ${playerOne}, computer has ${computerChoice} \nplayer one wins`);
-    }
-    else if (playerOne === 'scissors' && computerChoice === 'rock') {
-        alert(`player one has ${playerOne}, computer has ${computerChoice} \ncomputer wins`);
-    }
+let scores = {
+  win: 0,
+  tie: 0,
+  lose: 0,
+};
+function playGame(value) {
+  let playerOne = value;
+  let moves = ["rock", "paper", "scissors"];
+  let result;
+  let computerChoice = moves[Math.floor(Math.random() * moves.length)];
+  if (computerChoice === playerOne) {
+    scores.tie += 1;
+    result = "it's a tie"
+  } else if (playerOne === "rock" && computerChoice === "paper") {
+    scores.lose += 1;
+    result = "computer Wins"
+  } else if (playerOne === "rock" && computerChoice === "scissors") {
+    scores.win += 1;
+    result = "You Wins"
+  } else if (playerOne === "paper" && computerChoice === "scissors") {
+    scores.lose += 1;
+    result= "computer Wins"
+  } else if (playerOne === "paper" && computerChoice === "rock") {
+    scores.win += 1;
+    result = "You Wins"
+  } else if (playerOne === "scissors" && computerChoice === "paper") {
+    scores.win += 1;
+    result = "You Wins"
+  } else if (playerOne === "scissors" && computerChoice === "rock") {
+    scores.lose += 1;
+    result = "computer Wins"
+  }
+    alert(
+        `player one has ${playerOne}, computer has ${computerChoice} \n${result}\nwin: ${scores.win}, lose: ${scores.lose}, tie: ${scores.tie}`,
+    );
 }
-
+function clearScores() {
+    scores.lose = 0;
+    scores.tie = 0;
+    scores.win = 0;
+}
