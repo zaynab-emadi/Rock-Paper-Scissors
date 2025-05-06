3db2bade2fe2e5e9df385f4c1f5c0d23777bff84
zaynab_emadi <zaynabemadi@gmail.com>
2025-05-06 09:02:05 +0330

    add local storage for saving scores

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let scores = {
+let scores = JSON.parse(localStorage.getItem("scores")) || {
   win: 0,
   tie: 0,
   lose: 0,
@@ -10,32 +10,34 @@ function playGame(value) {
   let computerChoice = moves[Math.floor(Math.random() * moves.length)];
   if (computerChoice === playerOne) {
     scores.tie += 1;
-    result = "it's a tie"
+    result = "it's a tie";
   } else if (playerOne === "rock" && computerChoice === "paper") {
     scores.lose += 1;
-    result = "computer Wins"
+    result = "computer Wins";
   } else if (playerOne === "rock" && computerChoice === "scissors") {
     scores.win += 1;
-    result = "You Wins"
+    result = "You Wins";
   } else if (playerOne === "paper" && computerChoice === "scissors") {
     scores.lose += 1;
-    result= "computer Wins"
+    result = "computer Wins";
   } else if (playerOne === "paper" && computerChoice === "rock") {
     scores.win += 1;
-    result = "You Wins"
+    result = "You Wins";
   } else if (playerOne === "scissors" && computerChoice === "paper") {
     scores.win += 1;
-    result = "You Wins"
+    result = "You Wins";
   } else if (playerOne === "scissors" && computerChoice === "rock") {
     scores.lose += 1;
-    result = "computer Wins"
+    result = "computer Wins";
   }
-    alert(
-        `player one has ${playerOne}, computer has ${computerChoice} \n${result}\nwin: ${scores.win}, lose: ${scores.lose}, tie: ${scores.tie}`,
-    );
+  localStorage.setItem("scores", JSON.stringify(scores));
+  alert(
+    `player one has ${playerOne}, computer has ${computerChoice} \n${result}\nwin: ${scores.win}, lose: ${scores.lose}, tie: ${scores.tie}`,
+  );
 }
 function clearScores() {
-    scores.lose = 0;
-    scores.tie = 0;
-    scores.win = 0;
+  scores.lose = 0;
+  scores.tie = 0;
+  scores.win = 0;
+  localStorage.removeItem("scores");
 }
